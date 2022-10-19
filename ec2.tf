942e2f39169e228c10ae4275d775ba32825d1494
Ahmar Jenkins <104280394+Jstarri@users.noreply.github.com>
2022-10-18 20:01:11 -0400

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -6,14 +6,19 @@ resource "aws_instance" "web1" {
 
   # Security Group
   vpc_security_group_ids = ["${aws_security_group.ssh-allowed.id}"]
-  
+
   # the Public SSH key
-  key_name = "north-virginia-public-key-pair"
-  public_key = file("${var.PUBLIC_KEY_PATH}")
+  key_name = aws_key_pair.north-virginia-region-key-pair.id
 
   connection {
     host        = self.public_ip
     user        = var.EC2_USER
     private_key = file("${var.PRIVATE_KEY_PATH}")
   }
+}
+
+// Sends your public key to the instance
+resource "aws_key_pair" "north-virginia-region-key-pair" {
+  key_name   = "north-virginia-region-key-pair"
+  public_key = file("north-virginia-region-key-pair.pub")
 }
